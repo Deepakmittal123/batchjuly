8e2ca26a39802062c1f2f20cee88f7c42e4e3745
user <user@DESKTOP-6HCHEHQ>
2022-08-16 20:26:58 +0530

    Add project files.

---
+++ deepak/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deepak
+{
+    internal class car
+    //    {
+    //        public int[] GetArray()
+    //        {
+    //            int[] array1 = new int[5];
+
+    //            array1[0] = 10;
+    //            array1[1] = 20;
+    //            array1[2] = 30;
+    //            array1[3] = 40;
+    //            array1[4] = 50;
+
+    //            return array1;
+
+    //        }
+    //    }
+    //}
+    /////////////////////////////////////////
+    ///
+    //{
+    //public string GetCar1()
+    //{
+    //    string var = "clutch";
+    //    return var;
+    //}
+    //public string GetCar2()
+    //{
+    //    string var1 = "Break";
+    //    return var1;
+    //}
+    //public string GetCar3()
+    //{
+    //    string var2 = "accelator";
+    //    return var2;
+    //}
+
+
+
+
+
+    //}
+
+
+    //public string GetTelevison()
+    //    {
+    //        string var = "power button";
+    //        return var;
+    //    }
+    //    public string GetTelevison1()
+    //    { 
+    //        string var1 = "channel button";
+    //        return var1;
+    //    }
+    //    public string GetTelevison3()
+    //    { 
+    //        string var2 = "volume button";
+    //            return var2;
+
+
+    //    }
+    //}    
+    {
+        public string name { get; set; }
+
+
+        public int age { get; set; }
+    }
+
+}
+
+
+
+
+
---
+++ deepak/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace deepak
+
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //Class1 objmathsop = new Class1();
+
+            //int[] resultarray = objmathsop.GetArray();
+
+            //for(int i =0; i< resultarray.Length; i++)
+
+            //Console.WriteLine(i);
+
+
+
+            /////////////////////////////////////////////////////////////////////////////////////////
+            ///
+
+            //car objcar = new car();
+
+            //string var = objcar.GetCar1();
+
+            //Console.WriteLine("my output is " + var);
+
+            //string var1 = objcar.GetCar2();
+            //Console.WriteLine(var1);
+
+            //string var2 = objcar.GetCar3();
+            //Console.WriteLine(var2);
+
+
+            //car objcar = new car();
+
+            //string var = objcar.GetTeleviso
+            //Console.WriteLine(var);
+
+            //string var2 = objcar.GetTelevison1();
+            //Console.WriteLine(var2);
+
+            //string var3 = objcar.GetTelevison3();
+            //Console.WriteLine(var3);
+
+
+            //////////////////////////////////////////////////////////////////
+            ///
+            car myobj = new car();
+
+            myobj.name = "rahul";
+
+            myobj.age = 18;
+            Console.WriteLine("your name is " + myobj.name + "your age is " + myobj.age);
+            Console.ReadLine();
+        }
+    }
+}
